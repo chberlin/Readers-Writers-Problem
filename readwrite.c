43d74e382964548a6afaf3c50963f49b52a9068b
Charles Berlin <cberlin1@binghamton.edu>
2019-04-04 12:14:25 -0400

    starts thread creation. Not working

--- readwrite.c
+++ readwrite.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "linkedlist.c"
+#include <pthread.h>
+
+int createThreads(int numThreads, int num, void * (*start_func)(int));
+//input: Number of threads to create
+//input: num
+//input:function pointer to a starting function for the thread to run
+
+void *handleReaders(int num);
+
+void *handleWriters(int num);
+
+void *lastThread();
 
 int main(int argc, char ** argv, char ** envp) {
 
@@ -29,5 +41,43 @@ int main(int argc, char ** argv, char ** envp) {
 		exit(1);
 	}
 
+	//function pointer messed up
+	//can't figure out proper syntax
+	createThreads(numwriters, num, handleWriters);
+	createThreads(numreaders, num, handleReaders);
+	createThreads(1, 0, lastThread);
+	
+}
+
+int createThreads(int numThreads, int num, void * (*start_func){
+	pthread_t threads[numThreads];
+	void * retvals[numThreads];
+	int i;
+	for(i = 0; i < numThreads; i++){
+		if(pthread_create(&threads[i], NULL, start_func(int), num) != 0){
+			fprintf(stderr, "Error: Thread % %d cannot created\n", i);
+			exit(2);
+		}
+	}
+	for(int j = 0; j < i; j++){
+		if(pthread_join(threads[i], (void **)&retvals[i]) != 0){
+			fprintf(stderr, "Error: Thread %d cannot join\n", j);
+			exit(2);
+		}
+	}
+	return 0;
+}
+
+void *lastThread(){
+	//waits on condition variable when only single reader thread remains
+	fprintf(stdout, "Almost Done!\n");
+}
+
+void *handleReaders(int num){
 
 }
+
+void *handleWriters(int num){
+
+}
+	
